547fe56035c76c033277b21752c155f63dbc89ec
Rayan <anzairayan@gmail.com>
2022-03-18 00:30:17 -0300

    Update relogio.py

--- relogio.py
+++ relogio.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 import locale
-locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
-hoje = datetime.now()
 
-horario_final = hoje.strftime("%d/%B/%Y - %H:%M")
+locale.setlocale(locale.LC_ALL, 'pt_BR.utf8') #Localização BR.
+hoje = datetime.now() #variável com data deste momento
+
+horario_final = hoje.strftime("%d/%B/%Y - %H:%M") #Formatação para data, hora e minuto. %B em maiusculo para exibir nome do mês inteiro.
 
 print(horario_final)
 
